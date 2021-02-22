ed87a092296aba67c44e92e76d3e7c3b02e7d0f8
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-02-21 19:24:57 -0800

    Parse the port from args ?? use a default port

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "./deps.ts";
+import { Application, Router, parse } from "./deps.ts";
 import { list } from "./handlers/list.ts";
 import { create } from "./handlers/create.ts";
 import { remove } from "./handlers/remove.ts";
@@ -22,4 +22,7 @@ router
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+const DEFAULT_PORT = 8000;
+const argPort = parse(Deno.args).port;
+
+await app.listen({ port: argPort ?? DEFAULT_PORT });
